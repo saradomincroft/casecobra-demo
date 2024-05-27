c28d83016d8f8e30e63707f33df3c94f047f6919
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-05-27 21:56:39 +1000

    Added customer reviews to home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,8 +80,68 @@ export default function Home() {
                 <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500'/>
                 </span>
               {''} say</h2>
+              <img src='/snake-2.png' className='w-24 order-0 lg:order-2'/>
+            </div>
+
+            <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+              <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+                <div className='flex gap-0.5 mb-2'>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                </div>
+                <div className='text-lg leading-8'>
+                  <p>
+                    "The case feels durable and I event got a compliment on the design. Had the case for two and a half months now and <span className='p-0.5 bg-slate-800 text-white'>the image is super clear</span>, on the case I had before, the image started fading into a yellow-ish color after a couple of weeks. Love it."
+                  </p>
+                </div>
+                <div className='flex gap-4 mt-2'>
+                  <img className='rounded-full h-12 w-12 object-cover' src='/users/user-1.png' alt='user' />
+                  <div className='flex flex-col'>
+                    <p className='font-semibold'>Jonathan</p>
+                    <div className='flex gap-1.5 items-center text-zinc-600'></div>
+                      <Check className='h-4 w-4 stroke-[3px] text-green-600'/>
+                      <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Second user review */}
+            <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+              <div className='flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20'>
+                <div className='flex gap-0.5 mb-2'>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                  <Star className='h-5 w-5 text-green-600 fill-green-600'/>
+                </div>
+                <div className='text-lg leading-8'>
+                  <p>"I usually keep my phone together with my keys in my pocket and that led to some pretty heavy scratchmarks on all of my last
+                    phone cases. This one, besides a barely noticeable scratch on the corner, {''} <span className='p-0.5 bg-slate-800 text-white'>
+                      looks brand new after about half a year</span>. I dig it"
+                  </p>
+                </div>
+                <div className='flex gap-4 mt-2'>
+                  <img className='rounded-full h-12 w-12 object-cover' src='/users/user-4.jpg' alt='user' />
+                  <div className='flex flex-col'>
+                    <p className='font-semibold'>Josh</p>
+                    <div className='flex gap-1.5 items-center text-zinc-600'></div>
+                      <Check className='h-4 w-4 stroke-[3px] text-green-600'/>
+                      <p className='text-sm'>Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </MaxWidthWrapper>
+
+          <div className='pt-16'>
+            <Reviews />
+          </div>
+
       </section>
     </div>
   );
